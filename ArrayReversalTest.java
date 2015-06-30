ed0039fd399378a5a51d24d34f0d39ba96c2a5bc
petq01 <pmarinova0111@gmail.com>
2015-06-30 15:21:41 +0300

    first exercises java

---
+++ ArrayReversalTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ArrayReversalTest {
+
+	@Test
+	public void testReverseWithEvenLengthOfArray() {
+		int[] revArray = {1, 2, 3, 4, 5, 6}; 
+		ReverseClass.reverse(revArray); 
+		assertArrayEquals(new int[]{6, 5, 4, 3, 2, 1}, revArray);
+
+
+	}
+	@Test
+	public void testReverseWithOddLengthOfArray() {
+		int[] revArray = {1, 2, 3, 4, 5, 6, 7}; 
+		ReverseClass.reverse(revArray); 
+		assertArrayEquals(new int[]{7, 6, 5, 4, 3, 2, 1}, revArray);
+
+
+	}
+	@Test
+	public void testReverseWithEmptyArray() {
+		int[] revArray = {}; 
+		ReverseClass.reverse(revArray); 
+		assertArrayEquals(new int[]{}, revArray);
+	}
+	@Test
+	public void testReverseWithNullArray() {
+		int[] revArray = null; 
+		ReverseClass.reverse(revArray); 
+		assertArrayEquals(null, revArray);
+	}
+	@Test
+	public void testReverseWithJustOneElementArray() {
+		int[] revArray = {1}; 
+		ReverseClass.reverse(revArray); 
+		assertArrayEquals(new int[]{1}, revArray);
+	}
+
+}
---
+++ ReverseClass.java
@@ -0,0 +1,31 @@
+
+public class ReverseClass {
+
+	public static void main(String args[]) {
+		 
+		
+		int[] revArray = {-1,7,0, 3, 5, 6};
+		reverse(revArray);
+	}
+		public static void reverse( int[] revArray ){
+			try
+	        {
+             for (int i = 0; i < revArray.length / 2; i++)
+              {
+            	 //temporary variable which performs rotation in the loop
+                  int temp = revArray[revArray.length - 1 - i]; 
+                  
+                  revArray[revArray.length - 1 - i] = revArray[i]; 
+                  
+                  revArray[i] = temp;                    
+                  
+              }
+	        }catch (NullPointerException e) {
+				System.out.println("Null value!");
+			}
+            
+             for (int iterator : revArray){
+                 System.out.println(iterator+ " ");
+             }
+	}
+}
