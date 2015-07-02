6edba16c4943a087f150e26b5d15b9208c0dd382
petq01 <pmarinova0111@gmail.com>
2015-07-02 16:06:59 +0300

    some formatting in source folder

--- exer/Reverse/src/arrRev/ArrayReversal.java
+++ exer/Reverse/src/arrRev/ArrayReversal.java
@@ -3,30 +3,28 @@ package arrRev;
 public class ArrayReversal {
 
 	public static void main(String args[]) {
-		 
-		
-		int[] revArray = {-1,7,0, 3, 5, 6};
+
+		int[] revArray = { -1, 7, 0, 3, 5, 6 };
 		reverse(revArray);
 	}
-		public static void reverse( int[] revArray ){
-			try
-	        {
-             for (int i = 0; i < revArray.length / 2; i++)
-              {
-            	 //temporary variable which performs rotation in the loop
-                  int temp = revArray[revArray.length - 1 - i]; 
-                  
-                  revArray[revArray.length - 1 - i] = revArray[i]; 
-                  
-                  revArray[i] = temp;                    
-                  
-              }
-	        }catch (NullPointerException e) {
-				System.out.println("Null value!");
+
+	public static void reverse(int[] revArray) {
+		try {
+			for (int i = 0; i < revArray.length / 2; i++) {
+				// temporary variable which performs rotation in the loop
+				int temp = revArray[revArray.length - 1 - i];
+
+				revArray[revArray.length - 1 - i] = revArray[i];
+
+				revArray[i] = temp;
+
 			}
-            
-             for (int iterator : revArray){
-                 System.out.println(iterator+ " ");
-             }
+		} catch (NullPointerException e) {
+			System.out.println("Null value!");
+		}
+
+		for (int iterator : revArray) {
+			System.out.println(iterator + " ");
+		}
 	}
 }
--- exer/Reverse/src/arrRevTest/ArrayReversalTest.java
+++ exer/Reverse/src/arrRevTest/ArrayReversalTest.java
@@ -1,47 +1,48 @@
 package arrRevTest;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import arrRev.ArrayReversal;
 
-
-
 public class ArrayReversalTest {
 
 	@Test
 	public void testReverseWithEvenLengthOfArray() {
-		int[] revArray = {1, 2, 3, 4, 5, 6}; 
-		ArrayReversal.reverse(revArray); 
-		assertArrayEquals(new int[]{6, 5, 4, 3, 2, 1}, revArray);
-
+		int[] revArray = { 1, 2, 3, 4, 5, 6 };
+		ArrayReversal.reverse(revArray);
+		assertArrayEquals(new int[] { 6, 5, 4, 3, 2, 1 }, revArray);
 
 	}
+
 	@Test
 	public void testReverseWithOddLengthOfArray() {
-		int[] revArray = {1, 2, 3, 4, 5, 6, 7}; 
-		ArrayReversal.reverse(revArray); 
-		assertArrayEquals(new int[]{7, 6, 5, 4, 3, 2, 1}, revArray);
-
+		int[] revArray = { 1, 2, 3, 4, 5, 6, 7 };
+		ArrayReversal.reverse(revArray);
+		assertArrayEquals(new int[] { 7, 6, 5, 4, 3, 2, 1 }, revArray);
 
 	}
+
 	@Test
 	public void testReverseWithEmptyArray() {
-		int[] revArray = {}; 
-		ArrayReversal.reverse(revArray); 
-		assertArrayEquals(new int[]{}, revArray);
+		int[] revArray = {};
+		ArrayReversal.reverse(revArray);
+		assertArrayEquals(new int[] {}, revArray);
 	}
+
 	@Test
 	public void testReverseWithNullArray() {
-		int[] revArray = null; 
-		ArrayReversal.reverse(revArray); 
+		int[] revArray = null;
+		ArrayReversal.reverse(revArray);
 		assertArrayEquals(null, revArray);
 	}
+
 	@Test
 	public void testReverseWithJustOneElementArray() {
-		int[] revArray = {1}; 
-		ArrayReversal.reverse(revArray); 
-		assertArrayEquals(new int[]{1}, revArray);
+		int[] revArray = { 1 };
+		ArrayReversal.reverse(revArray);
+		assertArrayEquals(new int[] { 1 }, revArray);
 	}
 
 }
